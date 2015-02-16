dc15facb8bc58d3ac412b02c7c5a17149b0a673e
Karthikeyan S <keya@lostinkaos.com>
2015-02-16 09:10:15 +0530

    Version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'kaoskeya:admin',
-  version: '0.1.0',
+  version: '0.1.1',
   // Brief, one-line summary of the package.
   summary: 'Simple admin panel that integrates with your design.',
   // URL to the Git repository containing the source code for this package.
