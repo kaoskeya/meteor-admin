a1e6d5efc79f58416742e3697b1d365118d68b0b
Karthikeyan S <keya@lostinkaos.com>
2015-04-08 13:42:44 +0530

    Version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'kaoskeya:admin',
-  version: '0.1.8',
+  version: '0.1.9',
   // Brief, one-line summary of the package.
   summary: 'Simple autoform based admin that integrates with your bootstrap design.',
   // URL to the Git repository containing the source code for this package.
